4916bf884d2054922c8ac68caa69e17a5a68c18f
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-12-29 15:07:42 +0700

    .

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 
 // API to listen to Stripe Webhooks
-// app.post("/api/stripe",express.raw({ type: "application/json" }),stripeWebhooks);
+app.post(
+  "/api/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhooks
+);
 
 // Middleware to parse JSON
 app.use(express.json());
